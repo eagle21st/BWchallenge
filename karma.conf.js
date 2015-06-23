0b0a11f983de6b8272fac790b33e7c5320fba2c3
Yang Shu <yangs@punchtab.com>
2015-06-22 17:36:40 -0700

    change test files

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function(config) {
     files: [
       'bower_components/angular/angular.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'dist/double-progress.directive.js',
+      'double-progress.directive.js',
       'test/*.js'
     ],
 
